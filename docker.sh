37bcd85b9528b39de008d6ce98d2b9fe1a4d711a
Ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2024-04-15 23:26:04 -0400

    net

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
-docker build . -t dab_spam
-docker run --name dab_spammer dab_spam --restart=always -ti -d
+docker build . -t dab_spam --network=host
+docker run --name dab_spammer dab_spam --restart=always -ti -d 
